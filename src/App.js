e035d2b123b7388c7119a99267a892054cb40a37
Manuel Abrante <abrantemanuel@gmail.com>
2018-02-18 14:12:48 -0400

    updated app title

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">React Workshop</h1>
+          <h1 className="App-title">Reddit React Reader</h1>
         </header>
         {
           !this.props.isSelectionFinalized ?
